edf8b8383c87b8cd6f48b49935d6a48847ef7fc8
Daniil Shepitko <daniilshepitko7@gmail.com>
2024-12-05 11:29:12 +0100

    8

---
+++ lista d'exercicis 1.7/a 1.5/ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    const string MARCA_FI = null;  
+    static void Main()
+    {
+        
+        Random rand = new Random();
+        string line;
+        int totalTickets = 0;
+        int bonusCount = 0;
+        int totalAmount = 0;
+
+    
+        using (StreamReader reader = new StreamReader("bonus.txt"))
+        {
+            
+            line = reader.ReadLine();
+
+            while (line != MARCA_FI)
+            {
+                
+                totalTickets++;
+
+            
+                if (line == "BONUS")
+                {
+                    bonusCount++;
+                    int bonusAmount = rand.Next(1, 11);  
+                    totalAmount += bonusAmount;
+                }
+
+                
+                line = reader.ReadLine();
+            }
+        }
+
+       
+        if (totalTickets == 0)
+        {
+            Console.WriteLine("El fitxer està buit.");
+        }
+        else
+        {
+           
+            double bonusPercentage = (double)bonusCount / totalTickets * 100;
+  
+            Console.WriteLine($"Nombre total de bitllets: {totalTickets}");
+            Console.WriteLine($"Nombre de bitllets amb BONUS: {bonusCount}");
+            Console.WriteLine($"Percentatge de bitllets amb BONUS: {bonusPercentage:F2}%");
+            Console.WriteLine($"Import total dels guanys: {{totalAmount}}€");
+
+
+        }
+
+    }
+}
---
+++ lista d'exercicis 1.7/a 1.5/recoregut/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        const int MARCA_FI = 0;
+        int valor;
+        int nValors = 0;
+
+        valor = Convert.ToInt32(Console.ReadLine());
+   
+        while (valor != MARCA_FI)
+        {
+            nValors += 1; 
+            Console.WriteLine($"Interació {nValors} ---> {valor}"); 
+            valor = Convert.ToInt32(Console.ReadLine()); 
+        }
+
+        Console.WriteLine($"TOTAL VALORS INTRODUÏTS: {nValors}");
+    }
+}
